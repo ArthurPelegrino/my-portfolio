8dcbc8989cd9368221fd02ba6d11da83a492b8d0
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2024-09-12 09:50:31 -0300

    Criado estado global para dark mode e linguagens do site

---
+++ src/context/GlobalProvider.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import GlobalContext from './GlobalContext';
+
+type GlobalProviderProps = {
+  children: React.ReactNode;
+};
+
+function GlobalProvider({ children }: GlobalProviderProps) {
+  const [theme, setTheme] = useState<'dark' | 'light'>('dark');
+  // const [language, setLanguage] = useState<'en' | 'pt' | 'es'>('en');
+
+  function toggleTheme() {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  }
+
+  // function changeLanguage(newLanguage: 'en' | 'pt' | 'es') {
+  //   setLanguage(newLanguage);
+  // }
+
+  return (
+    <GlobalContext.Provider value={{ theme, toggleTheme
+    // , language,
+    //  setLanguage: changeLanguage 
+     }}>
+      <div>
+        {children}
+      </div>
+    </GlobalContext.Provider>
+  );
+}
+
+export default GlobalProvider;
